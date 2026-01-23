8b26b11c879b14132f02554d242afe5372d56167
sbcharr <sbcharr20@gmail.com>
2026-01-23 14:07:21 +0000

    format code block for better readability

--- src/main/java/com/github/sbcharr/jrpc/protocol/MessageHeader.java
+++ src/main/java/com/github/sbcharr/jrpc/protocol/MessageHeader.java
@@ -9,12 +9,18 @@ public class MessageHeader {
     public static final int HEADER_SIZE = 16;
     public static final byte VERSION = 1;
 
-    // wire fields
+    // ------wire fields--------
     private int magic = MAGIC_NUMBER; // Magic number -- 4 bytes
+
     private byte version = VERSION; // Protocol version -- 1 byte
+
     private MessageType messageType; // 0 = request, 1 = response -- 1 byte
+
     private SerializerType serializer; // 0 = JSON, 1 = Protobuf -- 1 byte
+
     private int payloadLength; // Length of the payload in bytes -- 4 bytes
+
     private int requestId; // Unique request ID -- 4 bytes
+
     private byte reserved = 0; // Reserved for future use -- 1 byte
 }
